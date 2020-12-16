289263ae6ed88ba6cf42137879907ae3a2bc0602
Buk <tanya.bukovkina@yandex.ru>
2020-12-16 15:46:10 +0300

    Add encryption/decription for string

--- Rabin/main.py
+++ Rabin/main.py
@@ -8,55 +8,61 @@ def gcdex(p, q):
 	return d, y, x - y * (p // q)
 
 
-def keygen():
+def keygen(m):
 	with open('prime_numbers.txt', 'r') as f:
 		str_prime = f.read()
-		prime_numbers = np.array([int(i) for i in str_prime.split(',')])
-	x = []
-	for i in prime_numbers:
-		if i % 4 == 3:
-			x.append(i)
-	
-	p = int(np.random.choice(x))
-	while True:
-		q = int(np.random.choice(x))
-		if q != p:
-			break
-
+		prime_numbers = np.array([int(i) for i in str_prime.split(',') 
+										 if int(i) % 4 == 3])
+	p = int(np.random.choice(prime_numbers))
+	q = int(np.random.choice(prime_numbers))
 	n = p * q
+	while q == p or n < m:
+		q = int(np.random.choice(prime_numbers))
+		n = p * q
 	print(f'p = {p}, q = {q}, n = {n}')
 	return p, q, n
 
 
-def encryption(n, M):
-	c = M ** 2 % n
+def encryption(n, m):
+	c = [mi ** 2 % n for mi in m]
 	return c
 
 
 def decryption(p, q, c):
 	n = p * q
-	d, yp, yq = gcdex(p, q)
+	# Вычисляем yp и yq по алгоритму Евклида:
+	_, yp, yq = gcdex(p, q)
 
-	mp = pow(c, (p+1)//4, p)
-	mq = pow(c, (q+1)//4, q)
+	mp = [pow(int(ci), (p+1)//4, p) for ci in c]
+	mq = [pow(int(ci), (q+1)//4, q) for ci in c]
 
-	m = []
-	m += [(yp * p * mq + yq * q * mp) % n]
-	m += [n - m[0]]
-	m += [(yp * p * mq - yq * q * mp) % n]
-	m += [n - m[2]]
+	m = np.zeros((4, len(c)))
+	m[0] = [(yp * p * mqi + yq * q * mpi) % n for mqi, mpi in zip(mq, mp)]
+	m[1] = n - m[0]
+	m[2] = [(yp * p * mqi - yq * q * mpi) % n for mqi, mpi in zip(mq, mp)]
+	m[3] = n - m[2]
+	m = m.T
 
-	return m
+	answer = ''
+	for row in m:
+		cur_chars = set([chr(int(i)) for i in row if i < 1111])
+		if len(cur_chars) == 1:
+			answer += list(cur_chars)[0]
+		else:
+			answer += '[' + ','.join(cur_chars) + ']'
+	return answer
 
 
 if __name__ == '__main__':
-	p, q, n = keygen()
+	m = input('Your message for encryption: ')
+	m = [ord(c) for c in m]
+	print('Message: ', m)
+	# Генерируем закрытый ключ (p,q) и открытый ключ n	
+	p, q, n = keygen(max(m))
 
-	M = 637284109 % n # M < n
-	print('M =', M)
+	c = encryption(n, m)
+	print(f'Ciphertext = {c}')
 
-	C = encryption(n, M)
-	print('C =', C)
-
-	m = decryption(p, q, C)
-	print(m)
+	m = decryption(p, q, c)
+	print(f'Found message: {m}')
+	
